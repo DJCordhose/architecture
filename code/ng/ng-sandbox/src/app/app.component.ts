3ae3490b15ce35adf6a4d7a8c09b92e134f57221
Oliver Zeigermann <oliver.zeigermann@gmail.com>
2018-05-29 14:13:17 +0200

    ng remote calls

--- code/ng/ng-sandbox/src/app/app.component.ts
+++ code/ng/ng-sandbox/src/app/app.component.ts
@@ -1,13 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { GreetingService } from "./greeting.service";
+import { Observable } from "rxjs";
+import { Message } from "./greeting.service";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent {
-  title = 'Hello';
+export class AppComponent implements OnInit {
+  constructor(private greetingService: GreetingService) {}
+
+  title = "Hello";
+  remoteGreeting: string;
+
   get chars() {
-    return ['H', 'e', 'l', 'l', 'o'];
+    const arr = [];
+    for (const c of this.title) {
+      arr.push(c);
+    }
+    return arr;
+    // return ["H", "e", "l", "l", "o"];
   }
+
+  ngOnInit(): void {
+    this.remote();
+  }
+
+  remote() {
+    this.greetingService
+      .requestGreeting(this.title)
+      .subscribe(data => (this.remoteGreeting = data.message));
 }
