77c15d1e09250bf86a347b2c60112eaa1abfe6e4
Oliver Zeigermann <oliver.zeigermann@gmail.com>
2018-05-30 08:23:56 +0200

    added @ngrx/store-devtools

--- code/ng/ng-sandbox/src/app/ngrx-sandbox/ngrx-sandbox.module.ts
+++ code/ng/ng-sandbox/src/app/ngrx-sandbox/ngrx-sandbox.module.ts
@@ -5,8 +5,17 @@ import { CounterComponent } from './counter.component';
 import { StoreModule } from '@ngrx/store';
 import { counterReducer } from './counter';
 
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../../environments/environment'; // Angular CLI environemnt
 @NgModule({
-  imports: [CommonModule, StoreModule.forRoot({ count: counterReducer })],
+  imports: [
+    CommonModule,
+    StoreModule.forRoot({ count: counterReducer }),
+  // Instrumentation must be imported after importing StoreModule (config is optional)
+  StoreDevtoolsModule.instrument({
+    maxAge: 25, // Retains last 25 states
+    logOnly: environment.production, // Restrict extension to log-only mode
+  })],
   declarations: [CounterComponent],
   exports: [CounterComponent]
 })
