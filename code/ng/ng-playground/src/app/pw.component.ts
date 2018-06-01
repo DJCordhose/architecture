3b422142e5ff4d4967a233748a4596358fd31b51
danielfernandez <daniel.fernandez2@soprasteria.com>
2018-06-01 10:03:21 +0200

    Added password component

---
+++ code/ng/ng-playground/src/app/pw.component.ts
@@ -0,0 +1,27 @@
+import { Component, Input, EventEmitter, Output } from '@angular/core';
+
+import {NavigationService, Menues} from './navigation.service';
+
+@Component({
+  selector: 'app-pw',
+  template: `<input type='password' [(ngModel)]='password1'>
+  <input type='password' [(ngModel)]='password2' (input)='onPw2Change()'>
+  <p style='color:red'>{{compare}}</p>`
+})
+export class PwComponent {
+  @Input() compare: string;
+  @Input() password2: string;
+  @Input() password1: string;
+  @Output() passwordEmiter = new  EventEmitter();
+
+  onPw2Change() {
+    if(this.password1!=this.password2) {
+      this.compare = 'Password do not match';
+    } else {
+      this.compare = '';
+      console.log('sending: '+this.password2);
+      this.passwordEmiter.emit({'password':this.password2});
+    }
+  }
+
+}
