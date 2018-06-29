4827880c564700dd02f39c09c1077bd2c5d03455
Oliver Zeigermann <oliver.zeigermann@gmail.com>
2018-06-29 17:40:26 +0200

    templates

--- code/ng/blueprint/src/app/app.component.ts
+++ code/ng/blueprint/src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  beers = ['astra', 'holsten', 'jever', 'ratsherren'];
+
+  update(title: string) {
+    console.log(title);
+    this.title = title;
+  }
 }
