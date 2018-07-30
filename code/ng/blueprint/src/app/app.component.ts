643cb24fb08dd0b69edf6dcbff9e1731e11230fa
Oliver Zeigermann <oliver.zeigermann@gmail.com>
2018-07-30 17:00:47 +0200

    added service to blueprint project

--- code/ng/blueprint/src/app/app.component.ts
+++ code/ng/blueprint/src/app/app.component.ts
@@ -1,16 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { GreetingService, Message } from './greeting.service';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
   beers = ['astra', 'holsten', 'jever', 'ratsherren'];
+  greeting: string;
+  response: Observable<string>;
+
+  constructor(private greetingService: GreetingService) {
+  }
 
   update(title: string) {
     console.log(title);
     this.title = title;
   }
+
+  ngOnInit(): void {
+    this.load();
+  }
+
+  load(): void {
+    this.response = this.greetingService.requestGreeting(this.title).pipe(map(greeting => greeting.message));
+    this.response.subscribe(greeting => this.greeting = greeting);
+  }
+
+
 }
---
+++ code/ng/blueprint/src/app/greeting.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+const urlBase = "http://localhost:3000";
+
+export interface Message {
+  message: string;
+}
+
+@Injectable({
+  providedIn: "root"
+})
+export class GreetingService {
+  constructor(private http: HttpClient) {}
+
+  requestGreeting(name: string): Observable<Message> {
+    const url = `${urlBase}/${name}`;
+    return this.http.get<Message>(url);
+  }
+}
