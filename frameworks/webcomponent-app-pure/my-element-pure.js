473c55c551eb9cda942d3846e3f5ee4a45d7b20c
Oliver Zeigermann <oliver.zeigermann@gmail.com>
2019-02-09 18:42:24 +0100

    pure web components and pwa

---
+++ frameworks/webcomponent-app-pure/my-element-pure.js
@@ -0,0 +1,47 @@
+const template = `
+<input id="in">
+<p><span id="log"></span>, World</p>
+<input id="btn" type="button" value="Clear">
+`;
+
+class MyElement extends HTMLElement {
+    static get observedAttributes() {
+        return ['greeting'];
+    }
+    constructor() {
+        super();
+        this.render();
+    }
+
+    connectedCallback() {
+        this.bind();
+        this.model = this.getAttribute("greeting");
+    }
+
+    bind() {
+        this.input.addEventListener("keyup", event => this.model = event.target.value);
+        this.button.addEventListener("click", event => {
+            this.model = "";
+            this.input.focus();
+        });
+    }
+
+    render() {
+        this.attachShadow({mode: 'open'});
+        this.shadowRoot.innerHTML = template;
+        this.input = this.shadowRoot.querySelector("#in");
+        this.log = this.shadowRoot.querySelector("#log");
+        this.button = this.shadowRoot.querySelector("#btn");
+    }
+    attributeChangedCallback(name, oldValue, newValue) {
+        if (name === "greeting") {
+            this.model = newValue;
+        }
+    }
+    set model(value) {
+        this.log.textContent = value;
+        this.input.value = value;
+    }
+}
+
+window.customElements.define('my-element', MyElement);
